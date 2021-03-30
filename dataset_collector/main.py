804b029d285699d5b62c915afe41ecf1a4997ee6
Pittawat <pittawat2542@gmail.com>
2021-03-30 22:33:26 +0700

    feat: dataset creator

--- dataset_collector/main.py
+++ dataset_collector/main.py
@@ -1,11 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium import webdriver
 
-base_url = 'https://pub.dev/'
 package_page_link_list = []
 github_link_list = []
 lib_link_list = []
 dart_file_link_list = []
+dart_file_raw_text_list = []
 
 
 def github_folder_traverse(page_link):
@@ -29,14 +29,18 @@ def github_folder_traverse(page_link):
 
 browser = webdriver.Chrome(executable_path='./chromedriver')
 
-for i in range(1, 2):  # TODO: Change back to 11
-    browser.get(f'{base_url}packages?sort=popularity&page={i}')
+for i in range(1, 11):
+    browser.get(f'https://pub.dev/packages?sort=popularity&page={i}')
     for package in browser.find_elements_by_css_selector(".packages-title a"):
         package_page_link_list.append(package.get_attribute("href"))
 
 for package_page in package_page_link_list:
     browser.get(package_page)
-    github_link_list.append(browser.find_element_by_link_text("Repository (GitHub)").get_attribute("href"))
+    try:
+        github_link = browser.find_element_by_link_text("Repository (GitHub)").get_attribute("href")
+        github_link_list.append(github_link)
+    except NoSuchElementException:
+        pass
 
 for github_page in github_link_list:
     browser.get(github_page)
@@ -48,7 +52,14 @@ for github_page in github_link_list:
 for lib_link in lib_link_list:
     github_folder_traverse(lib_link)
 
-print(dart_file_link_list)
-#TODO: Access URL, then download .dart file and store as .txt, also rename it with url name directly before blob/master
+for file_link in dart_file_link_list:
+    browser.get(file_link)
+    dart_file_raw_text_list.append(browser.find_element_by_id("raw-url").get_attribute("href"))
+
+with open("source_code.txt", "a") as file:
+    for source_code_link in dart_file_raw_text_list:
+        browser.get(source_code_link)
+        file.write(browser.find_element_by_tag_name("pre").text)
+        file.write("\n")
 
 browser.close()
